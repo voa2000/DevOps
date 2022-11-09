47710e16da4025065e1e62e509549adb44ae5295
Vivienne Ansah <voa2000@hotmail.com>
2022-11-09 01:30:59 +0000

    Update terraform test to jenkinsfile

---
+++ terraform/vpc.tf
@@ -0,0 +1,11 @@
+ #vpc.tf
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "VPC-demo"
+    Purpose = "terrafrom using Jenkins"
+  }
+}
